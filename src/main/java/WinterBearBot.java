602dd38c35844ab5b03b0096e777932443e99f17
WhiteBear82 <whitebearr82@gmail.com>
2025-01-19 00:44:41 +0800

    Completed Level-0, added WinterBearBot.java

---
+++ src/main/java/WinterBearBot.java
@@ -0,0 +1,14 @@
+public class WinterBearBot {
+    public static void DisplayWelcomeMessage() {
+        String welcomeMessage = "____________________________________________________________\n";
+        welcomeMessage += "Hello! I'm WinterBearBot\n";
+        welcomeMessage += "What can I do for you?\n";
+        welcomeMessage += "____________________________________________________________\n";
+        welcomeMessage += "Bye. Hope to see you again soon!\n";
+        welcomeMessage += "____________________________________________________________";
+        System.out.println(welcomeMessage);
+    }
+    public static void main(String[] args) {
+        DisplayWelcomeMessage();
+    }
+}
