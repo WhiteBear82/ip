c041d52eadde146d219b34d3aed106e0bb095736
WhiteBear82 <whitebearr82@gmail.com>
2025-01-19 11:09:26 +0800

    Completed Level-1, updated WinterBearBot.java

--- src/main/java/WinterBearBot.java
+++ src/main/java/WinterBearBot.java
@@ -1,14 +1,47 @@
+import java.util.Scanner;
+
 public class WinterBearBot {
+
+    private static String HORIZONTAL_LINE = "\t____________________________________________________________\n";
+
+    /**
+     * Level-0: Displays welcome message
+     */
     public static void DisplayWelcomeMessage() {
-        String welcomeMessage = "____________________________________________________________\n";
-        welcomeMessage += "Hello! I'm WinterBearBot\n";
-        welcomeMessage += "What can I do for you?\n";
-        welcomeMessage += "____________________________________________________________\n";
-        welcomeMessage += "Bye. Hope to see you again soon!\n";
-        welcomeMessage += "____________________________________________________________";
+        String welcomeMessage = HORIZONTAL_LINE;
+        welcomeMessage += "\tHello! I'm WinterBearBot\n";
+        welcomeMessage += "\tWhat can I do for you?\n";
+        welcomeMessage += HORIZONTAL_LINE;
         System.out.println(welcomeMessage);
     }
+
+    /**
+     * Level-1: Echo user commands (inputs)
+     */
+    public static void EchoInput() {
+        String input = "";
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye"))
+                break;
+            System.out.println(HORIZONTAL_LINE + "\t" + input + "\n" + HORIZONTAL_LINE);
+        }
+    }
+
+    /**
+     * Display message before the end of the program
+     */
+    public static void DisplayFarewellMessage() {
+        String farewellMessage = HORIZONTAL_LINE;
+        farewellMessage += "\tBye. Hope to see you again soon!\n";
+        farewellMessage += HORIZONTAL_LINE;
+        System.out.println(farewellMessage);
+    }
+
     public static void main(String[] args) {
         DisplayWelcomeMessage();
+        EchoInput();
+        DisplayFarewellMessage();
     }
 }
